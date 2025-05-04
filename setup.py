8954cca663323ac1c6ace054bb7f4309f6b922d8
Andrew Johnson <andrewmjohnson549@gmail.com>
2025-05-04 13:28:52 +0200

    Improve naming

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 from setuptools import find_packages, setup
 
-package_name = 'serial_reader'
+package_name = "serial_reader"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools', "pyserial"],
+    install_requires=["setuptools", "pyserial"],
     zip_safe=True,
-    maintainer='Andrew Johnson',
-    maintainer_email='andrewmjohnson549@gmail.com',
-    description='Contains the a node for reading serial data and publishing to a topic',
-    license='Apache',
-    tests_require=['pytest'],
+    maintainer="Andrew Johnson",
+    maintainer_email="andrewmjohnson549@gmail.com",
+    description="Contains the a node for reading serial data and publishing to a topic",
+    license="Apache",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'publisher = serial_reader.publisher: main',
+        "console_scripts": [
+            "serial_reader = serial_reader.publisher: main",
         ],
     },
 )
